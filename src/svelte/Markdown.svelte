3ff6907a64ba74192c6d96508e919e3d30e810e3
gregpriday <greg@siteorigin.com>
2024-08-16 11:28:02 +0200

    Fixed markdown component

--- src/svelte/Markdown.svelte
+++ src/svelte/Markdown.svelte
@@ -1,67 +1,26 @@
 <script>
     import { marked } from 'marked';
-    import ResponsiveImage from "./ResponsiveImage.svelte";
 
-    export let content = '';
-    export let componentDefaults = {};
-
-    function parseContent(content) {
-        const regex = /^<(\w+)\s+({.*?})\s*\/>$/gm;
-        const parts = [];
-        let lastIndex = 0;
-        let match;
-
-        while ((match = regex.exec(content)) !== null) {
-            // Add any preceding text as a markdown part
-            if (match.index > lastIndex) {
-                parts.push({
-                    type: 'markdown',
-                    content: content.slice(lastIndex, match.index)
-                });
-            }
-
-            // Add the component part
-            const componentName = match[1];
-            const componentProps = JSON.parse(match[2]);
-
-            // Merge default props with component-specific props
-            const mergedProps = {
-                ...componentDefaults[componentName],
-                ...componentProps
-            };
-
-            parts.push({
-                type: 'component',
-                name: componentName,
-                props: mergedProps
-            });
-
-            lastIndex = regex.lastIndex;
-        }
-
-        // Add any remaining text as a markdown part
-        if (lastIndex < content.length) {
-            parts.push({
-                type: 'markdown',
-                content: content.slice(lastIndex)
-            });
-        }
-
-        return parts;
-    }
-
-    $: contentParts = parseContent(content);
+    export let content = [];
+    export let components = {};
 </script>
 
-{#each contentParts as part}
-    {#if part.type === 'markdown'}
-        {@html marked(part.content)}
-    {:else if part.type === 'component'}
-        {#if part.name === 'ResponsiveImage'}
-            <ResponsiveImage {...part.props} />
+{#each content as item}
+    {#if item.type === 'markdown'}
+        {@html marked(item.content)}
+    {:else if item.type === 'component'}
+        {#if components[item.name]}
+            <svelte:component this={components[item.name]} {...item.props}>
+                {#if item.children}
+                    <svelte:self content={item.children} {components} />
+                {/if}
+            </svelte:component>
         {:else}
-            <!-- Handle unknown components -->
-            <p>Unknown component: {part.name}</p>
+            <svelte:element this={item.name} {...item.props}>
+                {#if item.children}
+                    <svelte:self content={item.children} {components} />
+                {/if}
+            </svelte:element>
         {/if}
     {/if}
 {/each}
